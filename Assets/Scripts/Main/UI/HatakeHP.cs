1c8c293922ef5dc9a2b9c2de1faece8c18fb1687
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-06 01:20:05 +0900

    Add: HP Gauge

---
+++ Assets/Scripts/Main/UI/HatakeHP.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HatakeHP : MonoBehaviour
+{
+    [SerializeField] private Image healthImage;
+    [SerializeField] private Image burnImage;
+
+    public float duration = 0.5f;
+    public float strength = 20f;
+    public int vibrate = 100;
+
+    public float debugDamageRate = 0.1f;
+
+    private float currentRate = 1f;
+
+    private void Start()
+    {
+        SetGauge(1f);
+    }
+
+    public void SetGauge(float value)
+    {
+        // DoTweenを連結して動かす
+        healthImage.DOFillAmount(value, duration)
+            .OnComplete(() =>
+            {
+                burnImage
+                    .DOFillAmount(value, duration / 2f)
+                    .SetDelay(0.5f);
+            });
+        transform.DOShakePosition(
+            duration / 2f,
+            strength, vibrate);
+
+        currentRate = value;
+    }
+
+    public void TakeDamage(float rate)
+    {
+        SetGauge(currentRate - rate);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(debugDamageRate);
+        }
+    }
+}
